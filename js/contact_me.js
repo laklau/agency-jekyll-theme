728b174625550cb21a0c5117f5881d6117099e42
Klaudia Alvarez <klaudia.devel@gmail.com>
2016-11-26 17:41:59 +0100

    Clean up before theme Jekyllization

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "",
                 type: "POST",
                 data: {
                     name: name,
